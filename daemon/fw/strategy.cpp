58c02a5a19980b719c4417ceef6123d3e1d4593c
Marcin Bosk <m.bosk@tum.de>
2024-05-23 20:18:09 +0200

    Fixed unchanged legacy variable name

--- daemon/fw/strategy.cpp
+++ daemon/fw/strategy.cpp
@@ -254,11 +254,11 @@ Strategy::sendData(const Data& data, Face& egress, const shared_ptr<pit::Entry>&
     pitToken = inRecord->getInterest().getTag<lp::PitToken>();
   }
 
-    // delete the PIT entry's in-record based on egress,
-    // since the Data is sent to the face from which the Interest was received
-    if (!pitEntry->isSoftState || pitEntry->isExpired) {
+  // delete the PIT entry's in-record based on egress,
+  // since the Data is sent to the face from which the Interest was received
+  if (!pitEntry->isSoftState || pitEntry->isExpired) {
     NFD_LOG_DEBUG("sendData, interest=" << pitEntry->getName() << " " << pitEntry->isSoftState << " " << pitEntry->isExpired);
-    pitEntry->deleteInRecord(egress);
+    pitEntry->deleteInRecord(inRecord);
   } else {
     NFD_LOG_DEBUG("sendData, interest=" << pitEntry->getName() << " was soft state. Not deleting inRecord");
   }
